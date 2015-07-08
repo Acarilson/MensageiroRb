faad1ea9d185949a689f1f5354293eae80384cb5
Ylan Adlin <ylanqs@gmail.com>
2015-07-08 15:06:16 -0300

    Teste de inserções

--- server-central.rb
+++ server-central.rb
@@ -1,5 +1,7 @@
 require 'socket'
+require 'rdbi-driver-sqlite3'
 
+dbh = RDBI.connect(:SQLite3, :database => "bd.db")
 socket = UDPSocket.new
 socket.bind("",2100)
 loop{
@@ -7,6 +9,18 @@ loop{
         sender_ip = sender[3]
         sender_port = sender[1]
         puts "Dados recebidos do cliente #{sender_ip}:#{sender_port}: #{data}"
-        socket.send("Mensagem de resposta", 0, sender_ip, sender_port)
+
+        #Verificar comando
+        data = data.split
+        if (data[0] == "REG")
+        	dbh.execute('insert into pclocal (dominio, ip) values ("' + data[1] + '", "' + data[2] + '")')
+        	socket.send("REGOK", 0, sender_ip, sender_port)
+        elsif (data[0] == "IP")
+        	socket.send("IP", 0, sender_ip, sender_port)
+        else
+        	socket.send("FALHA", 0, sender_ip, sender_port)
+        end
 }
+
 socket.close
+dbh.disconnect
