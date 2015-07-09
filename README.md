fac4eb5aec65a8c1b97cbbb1c779b0da69d6eefc
Tiago Dantas <ylanqs@gmail.com>
2015-07-09 12:04:33 -0300

    Registros e consulta de DNS, OK! Quando houver requisição de dominio o IP será informado

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Fontes
 =======
 
 Slide "Acessando bancos de dados relacionais com Ruby e RDBI" do prof. Alexandre Gomes
+
+Comandos Github: http://rogerdudler.github.io/git-guide/index.pt_BR.html
--- server-central.rb
+++ server-central.rb
@@ -4,6 +4,7 @@ require 'rdbi-driver-sqlite3'
 dbh = RDBI.connect(:SQLite3, :database => "bd.db")
 socket = UDPSocket.new
 socket.bind("",2100)
+
 loop{
         data, sender = socket.recvfrom(1024)
         sender_ip = sender[3]
@@ -13,10 +14,22 @@ loop{
         #Verificar comando
         data = data.split
         if (data[0] == "REG")
-        	dbh.execute('insert into pclocal (dominio, ip) values ("' + data[1] + '", "' + data[2] + '")')
-        	socket.send("REGOK", 0, sender_ip, sender_port)
+    		rs_dom_ip = dbh.execute("select * from pclocal where dominio = '" + data[1] + "' or ip = '" + data[2] + "'")
+    		if (rs_dom_ip.count != 0)
+    			socket.send("REGFALHA", 0, sender_ip, sender_port)
+    		else
+    			dbh.execute('insert into pclocal (dominio, ip) values ("' + data[1] + '", "' + data[2] + '")')
+    			socket.send("REGOK", 0, sender_ip, sender_port)
+    		end
         elsif (data[0] == "IP")
-        	socket.send("IP", 0, sender_ip, sender_port)
+        	rs_dom = dbh.execute("select * from pclocal where dominio = '" + data[1] + "'")
+        	if (rs_dom.count != 0)
+        		rs_dom.fetch(:all, :Struct).each do |row|
+					socket.send("IPOK " + row.ip, 0, sender_ip, sender_port)
+				end
+        	else
+        		socket.send("IPFALHA", 0, sender_ip, sender_port)
+        	end
         else
         	socket.send("FALHA", 0, sender_ip, sender_port)
         end
