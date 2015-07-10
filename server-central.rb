0a0a8ad94d45a13ed2e31a7b3a06b7e446f6ac1f
Tiago Dantas <ylanqs@gmail.com>
2015-07-10 11:49:27 -0300

    Testes

--- server-central.rb
+++ server-central.rb
@@ -13,25 +13,25 @@ loop{
 
         #Verificar comando
         data = data.split
-        if (data[0] == "REG")
-    		rs_dom_ip = dbh.execute("select * from pclocal where dominio = '" + data[1] + "' or ip = '" + data[2] + "'")
-    		if (rs_dom_ip.count != 0)
-    			socket.send("REGFALHA", 0, sender_ip, sender_port)
-    		else
-    			dbh.execute('insert into pclocal (dominio, ip) values ("' + data[1] + '", "' + data[2] + '")')
-    			socket.send("REGOK", 0, sender_ip, sender_port)
-    		end
-        elsif (data[0] == "IP")
-        	rs_dom = dbh.execute("select * from pclocal where dominio = '" + data[1] + "'")
-        	if (rs_dom.count != 0)
-        		rs_dom.fetch(:all, :Struct).each do |row|
-					socket.send("IPOK " + row.ip, 0, sender_ip, sender_port)
-				end
-        	else
-        		socket.send("IPFALHA", 0, sender_ip, sender_port)
-        	end
+        if (data[0] == "REG" and data.count == 3)
+            rs_dom_ip = dbh.execute("select * from pclocal where dominio = '" + data[1] + "' or ip = '" + data[2] + "'")
+            if (rs_dom_ip.count != 0)
+                socket.send("REGFALHA", 0, sender_ip, sender_port)
+            else
+                dbh.execute('insert into pclocal (dominio, ip) values ("' + data[1] + '", "' + data[2] + '")')
+                socket.send("REGOK", 0, sender_ip, sender_port)
+            end
+        elsif (data[0] == "IP" and data.count == 2)
+            rs_dom = dbh.execute("select * from pclocal where dominio = '" + data[1] + "'")
+            if (rs_dom.count != 0)
+                    rs_dom.fetch(:all, :Struct).each do |row|
+                    socket.send("IPOK " + row.ip, 0, sender_ip, sender_port)
+                end
+            else
+                socket.send("IPFALHA", 0, sender_ip, sender_port)
+            end
         else
-        	socket.send("FALHA", 0, sender_ip, sender_port)
+            socket.send("FALHA", 0, sender_ip, sender_port)
         end
 }
 
